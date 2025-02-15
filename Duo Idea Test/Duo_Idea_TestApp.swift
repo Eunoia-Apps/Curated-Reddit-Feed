d515533b6e845a9df4abd73e22b6b00779edbb9c
Eba <121199967+spac3-dust@users.noreply.github.com>
2025-02-15 14:08:07 +0900

    Initial Commit

---
+++ Duo Idea Test/Duo_Idea_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Duo_Idea_TestApp.swift
+//  Duo Idea Test
+//
+//  Created by Abe on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Duo_Idea_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
